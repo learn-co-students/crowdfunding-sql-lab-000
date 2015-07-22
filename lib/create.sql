5074affe63832797738ab76e757c1220c8390592
mbrickenworthe <adamwinters456@gmail.com>
2015-07-22 18:02:13 -0400

    Create tables and doing advanced SQL queries to return results

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,21 @@
--- your code here
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  amount INTEGER,
+  user_id INTEGER REFERENCES user,
+  project_id INTEGER REFERENCES project
+);
+
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,13 @@
--- your code here
+INSERT INTO users (name, age) VALUES (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale"), (24, "Gale");
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008), ("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008),
+("This is the title", "Writing script", 20000, 10/1/2008, 10/31/2008);
+
+INSERT INTO pledges (amount) VALUES (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345), (12345);
